7d7157d49bd2163a6eb84cab3c202ff0f73d4af1
Arnold, Baker D. (S&T-Student) <adbnm4@mst.edu>
2019-06-07 12:33:13 -0500

    Messing around with editing .csv files

--- csvTest.py
+++ csvTest.py
@@ -4,13 +4,14 @@ with open('profiles.csv', 'r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
     for line in csv_reader:
-        # print(line['user'])
-        if 'user' in line['user']:
-            print('yes')
+        print(line['user'])
+        # if 'user' in line['user']:
+        #     print('yes')
 
     with open('new_names.csv', 'w') as new_file:
         fieldnames = ['user', 'password']
-        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
 
         csv_writer.writeheader()
 
