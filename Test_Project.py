e088208720119170de0948bbfa73f8f1e5cb187f
Arnold, Baker D. (S&T-Student) <adbnm4@mst.edu>
2019-06-19 15:49:32 -0500

    Adding some garbage so git will let me switch branches

--- Test_Project.py
+++ Test_Project.py
@@ -3,12 +3,12 @@ import csv
 
 
 # ***** MAIN LOGIN WINDOW *****
-class LoginWindow(tk.Frame):
+class LoginWindow:
     # Create a class called Application w/ parent tk.Frame
 
-    def __init__(self):
-        tk.Frame.__init__(self)
-        self.root = tk.Tk()
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
 
         # ***** Generate Users *****
         # Sets basic username/password parameters
@@ -25,7 +25,7 @@ class LoginWindow(tk.Frame):
 
         # ***** Create Logo *****
         # Create the logo at the top of the screen
-        self.logo = tk.Label(self)
+        self.logo = tk.Label(self.frame)
         self.logo["text"] = "Login Application"
         self.logo["font"] = ('Times New Roman', 48)
         self.logo["bd"] = 1
@@ -35,60 +35,67 @@ class LoginWindow(tk.Frame):
         self.logo.grid(columnspan=2)
 
         # ***** Create Entries *****
-        self.user_label = tk.Label(self, text="Username:")
+        self.user_label = tk.Label(self.frame, text="Username:")
         self.user_label.grid(column=0, row=1)
 
-        self.user_entry = tk.Entry(self)
+        self.user_entry = tk.Entry(self.frame)
         self.user_entry.grid(column=1, row=1)
 
-        self.password_label = tk.Label(self, text="Password:")
+        self.password_label = tk.Label(self.frame, text="Password:")
         self.password_label.grid(column=0, row=2)
 
-        self.password_entry = tk.Entry(self)
+        self.password_entry = tk.Entry(self.frame)
         self.password_entry.grid(column=1, row=2)
 
+        # ***** Create Newuser Button *****
+        self.newuser_button = tk.Button(self.frame, text="New user? Click here.", command=self.newuser)
+        self.newuser_button.grid(columnspan=2, row=4)
+
+        # ***** Pack the Frame *****
+        self.frame.pack()
+
         # ***** Create Login Button *****
-        def login(event):
-            # Create a login function that checks for user input
+    def login(self):
+        # Create a login function that checks for user input
 
-            if self.user_entry.get() in LoginWindow.users.keys():
-                if self.password_entry.get() == LoginWindow.users[self.user_entry.get()]:
-                    print("Logged in!")
-                else:
-                    print("Incorrect password. Please try again.")
+        if self.user_entry.get() in LoginWindow.users.keys():
+            if self.password_entry.get() == LoginWindow.users[self.user_entry.get()]:
+                print("Logged in!")
             else:
-                print("That username doesn't exist. Please try again.")
+                print("Incorrect password. Please try again.")
+        else:
+            print("That username doesn't exist. Please try again.")
 
-        self.login_button = tk.Button(self, text="Login")
-        self.login_button.bind("<Button-1>", login)
+        self.login_button = tk.Button(self.frame, text="Login", command=self.login)
         self.login_button.grid(columnspan=2, row=3)
 
         # ***** Create New User Button *****
-        def newuser(event):
-            app1 = NewUserWindow()
-            app1.mainloop()
+    def newuser(self):
+        def newuser_main():
+            root = tk.Tk()
+            app = NewUserWindow(root)
+            root.mainloop()
 
-        self.newuser_button = tk.Button(self, text="New user? Click here.")
-        self.newuser_button.bind("<Button-1>", newuser)
-        self.newuser_button.grid(columnspan=2, row=4)
+        if __name__ == '__main__':
+            newuser_main()
 
 
 # ***** NEW USER WINDOW *****
-class NewUserWindow(tk.Frame):
+class NewUserWindow:
     # A new window for creating a new user profile
 
-    def __init__(self):
-        super().__init__()
-        self.root = tk.Tk()
-        self.create_logo()
-        self.create_entries()
-        self.confirmation_button()
-        self.root.mainloop()
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        # self.root = tk.Tk()
+        # self.create_logo()
+        # self.create_entries()
+        # self.confirmation_button()
+        # self.root.mainloop()
 
-    def create_logo(self):
+        # ***** Create Logo *****
         # Create the logo at the top of the screen
-
-        self.logo = tk.Label(self)
+        self.logo = tk.Label(self.frame)
         self.logo["text"] = "Welcome stranger!"
         self.logo["font"] = ('Times New Roman', 36)
         self.logo["bd"] = 1
@@ -97,57 +104,58 @@ class NewUserWindow(tk.Frame):
         # flat, groove, raised, ridge, solid, or sunken
         self.logo.grid(columnspan=2)
 
-    def create_entries(self):
+        # ***** Create Entries *****
         # Creates labels and entries
-
-        self.user_label = tk.Label(self, text="Create username:")
+        self.user_label = tk.Label(self.frame, text="Create username:")
         self.user_label.grid(column=0, row=1, sticky='E')
 
-        self.user_entry = tk.Entry(self)
+        self.user_entry = tk.Entry(self.frame)
         self.user_entry.grid(column=1, row=1)
 
-        self.password_label = tk.Label(self, text="Create password:")
+        self.password_label = tk.Label(self.frame, text="Create password:")
         self.password_label.grid(column=0, row=2, sticky='E')
 
-        self.password_entry = tk.Entry(self)
+        self.password_entry = tk.Entry(self.frame)
         self.password_entry.grid(column=1, row=2)
 
-    def confirmation_button(self):
+        # Create the confirmation buttons
+        self.confirm_button = tk.Button(self.frame, text="Confirm", command=self.confirm)
+        self.confirm_button.grid(columnspan=2, row=3)
+        self.frame.pack()
+        # ***** Confirmation Button *****
         # Button that confirms the selection and adds the
         # entry into the dictionary
 
-        def confirm(event):
-            temp_user = self.user_entry.get()
-            temp_password = self.password_entry.get()
+    def confirm(self):
+        temp_user = self.user_entry.get()
+        temp_password = self.password_entry.get()
 
-            if temp_user in LoginWindow.users.keys():
-                # Check to see if the username is already taken
-                print(f"{self.user_entry.get()} already taken.")
-            elif temp_user == '':
-                print("You didn't enter a username ya ding dong!")
-            elif temp_password == '':
-                print("You didn't enter a password dummy!")
-            else:
-                # Add a new dictionary entry for the given input
-                LoginWindow.users[temp_user] = temp_password
+        if temp_user in LoginWindow.users.keys():
+            # Check to see if the username is already taken
+            print(f"{self.user_entry.get()} already taken.")
+        elif temp_user == '':
+            print("You didn't enter a username ya ding dong!")
+        elif temp_password == '':
+            print("You didn't enter a password dummy!")
+        else:
+            # Add a new dictionary entry for the given input
+            LoginWindow.users[temp_user] = temp_password
 
-                with open('profiles.csv', 'a+', newline='') as new_file:
-                    fieldnames = ['user', 'password']
+            with open('profiles.csv', 'a+', newline='') as new_file:
+                fieldnames = ['user', 'password']
 
-                    csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+                csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
 
-                    csv_writer.writerow({'user': temp_user, 'password': temp_password})
+                csv_writer.writerow({'user': temp_user, 'password': temp_password})
 
-                print("New profile created successfully")
-                # Need to add a line containing user entry to the profiles.csv file
-                # and then call the generate_users function from the LoginWindow class
-                # in order to regenerate the dictionary that the program looks at to
-                # verify if a user is in the system or not
+            print("New profile created successfully")
+            self.master.destroy()
 
-        # Create the confirmation buttons
-        self.confirm_button = tk.Button(self, text="Confirm")
-        self.confirm_button.bind("<Button-1>", confirm)
-        self.confirm_button.grid(columnspan=2, row=3)
 
-app = LoginWindow()
-# Run the program
+def main():
+    root = tk.Tk()
+    app = LoginWindow(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ testFile2.py
@@ -0,0 +1,32 @@
+# Putting each of your top-level windows into it's own separate class gives you code re-use and better code organization. Any buttons and relevant methods that are present in the window should be defined inside this class. Here's an example (taken from here):
+
+import tkinter as tk
+
+class Demo1:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.button1 = tk.Button(self.frame, text = 'New Window', width=25, command = self.new_window)
+        self.button1.pack()
+        self.frame.pack()
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Demo2(self.newWindow)
+
+class Demo2:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(self.master)
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command=self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+    def close_windows(self):
+        self.master.destroy()
+
+def main():
+    root = tk.Tk()
+    app = Demo1(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
